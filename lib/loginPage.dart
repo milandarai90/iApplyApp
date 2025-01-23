87712596da92d632b25ca9dbacfd4398a1dc9116
milandarai90 <milandarai90@gmail.com>
2025-01-24 02:45:56 +0545

    signup form done

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:iapply/ui_helper.dart';
+import 'package:iapply/signupPage.dart';
+
 
 class LoginPage extends StatefulWidget{
   @override
@@ -19,133 +20,139 @@ class LoginPageState extends State<LoginPage>{
      //   title: Text(''),
      // ),
      body: Container(
+       height: MediaQuery.of(context).size.height,
        color: Theme.of(context).primaryColor,
-       child: Padding(
-         padding: const EdgeInsets.all(20.0),
-         child: Column(
-           children: [
-             Padding(
-               padding: const EdgeInsets.all(60.0),
-               child: Image.asset('assets/images/img.png'),
-             ),
-             Padding(
-               padding: const EdgeInsets.all(10.0),
-               child: Container(
+       child: SingleChildScrollView(
+         child: Padding(
+           padding: const EdgeInsets.all(20.0),
+           child: Column(
+             children: [
+               Padding(
+                 padding: const EdgeInsets.all(60.0),
+                 child: Image.asset('assets/images/img.png'),
+               ),
+               Padding(
+                 padding: const EdgeInsets.all(10.0),
+                 child: Container(
+                   decoration: BoxDecoration(
+                       color: Theme.of(context).canvasColor,
+                     borderRadius: BorderRadius.circular(10)
+                   ),
 
-                 color: Theme.of(context).canvasColor,
-                 child: Form(
-                   child: Padding(
-                     padding: const EdgeInsets.all(20.0),
-                     child: Column(
-                       children: [
-                         Text('Login',style: TextStyle(fontSize: 30,color: Theme.of(context).primaryColor, fontWeight: FontWeight.bold),),
-                         SizedBox(
-                           height: 20,
-                         ),
-                         TextFormField(
-                           controller: emailController,
-                           style: TextStyle(color: Colors.black),
-                           decoration: InputDecoration(
+                   child: Form(
+                     child: Padding(
+                       padding: const EdgeInsets.all(20.0),
+                       child: Column(
+                         children: [
+                           Text('Login',style: TextStyle(fontSize: 30,color: Theme.of(context).primaryColor, fontWeight: FontWeight.w800),),
+                           SizedBox(
+                             height: 20,
+                           ),
+                           TextFormField(
+                             controller: emailController,
+                             style: TextStyle(color: Colors.black),
+                             decoration: InputDecoration(
+                                 enabledBorder: OutlineInputBorder(
+                                   borderSide: BorderSide(
+                                     color: Colors.white,
+                                     // width: 2.0,
+                                   ),
+                                   borderRadius: BorderRadius.circular(10.0)
+                                 ),
+                                 focusedBorder: OutlineInputBorder(
+                                   borderRadius: BorderRadius.circular(10.0),
+                                   borderSide: BorderSide(
+                                     color: Theme.of(context).primaryColor,
+                                     width: 2.0,
+                                   )
+                                 ),
+                                 prefixIcon: Icon(Icons.email,color: Theme.of(context).primaryColor,),
+                                 hintText: "Enter your email.",
+                                 hintStyle: TextStyle(color: Colors.grey, fontSize: 16),
+                               fillColor: Colors.white,
+                               filled: true,
+                             ),
+                           ),
+                           SizedBox(
+                             height: 20,
+                           ),
+                           TextFormField(
+                             controller: passwordController,
+                             decoration: InputDecoration(
                                enabledBorder: OutlineInputBorder(
                                  borderSide: BorderSide(
                                    color: Colors.white,
-                                   // width: 2.0,
+                                   // width: 2.0
                                  ),
-                                 borderRadius: BorderRadius.circular(10.0)
+                                   borderRadius: BorderRadius.circular(10.0),
                                ),
                                focusedBorder: OutlineInputBorder(
                                  borderRadius: BorderRadius.circular(10.0),
                                  borderSide: BorderSide(
-                                   color: Colors.white,
-                                   width: 2.0,
+                                   color: Theme.of(context).primaryColor,
+                                   width: 2,
+                                 ),
+                               ),
+                               prefixIcon: Icon(Icons.key,color: Theme.of(context).primaryColor,),
+                               hintText: "Enter your password.",
+                               hintStyle: TextStyle(color: Colors.grey,fontSize: 16),
+                               suffixIcon: IconButton(onPressed: (){},icon: Icon(Icons.remove_red_eye),),
+                               suffixIconColor: Theme.of(context).primaryColor,
+                               fillColor: Colors.white,
+                               filled: true,
+         
+                             ),
+                             obscureText: true,
+                             obscuringCharacter: '*',
+                           ),
+                           SizedBox(
+                            height: 10,
+                          ),
+                           TextButton(onPressed: (){
+         // something here
+                           }, child: Text('Forgot Password?',style: TextStyle(color: Theme.of(context).primaryColor , fontSize: 14),)),
+                           SizedBox(
+                             height: 10
+                           ),
+                           Container(
+                             width: 120,
+                             child: ElevatedButton(onPressed: (){
+                               String userEmail = emailController.text.toString();
+                               String userPassword = passwordController.text.toString();
+         
+                             //some operation to be done here...
+         
+                             },
+                               style: ElevatedButton.styleFrom(
+                                 backgroundColor: Theme.of(context).primaryColor,
+                                 shape: RoundedRectangleBorder(
+                                   borderRadius: BorderRadius.circular(10)
                                  )
                                ),
-                               prefixIcon: Icon(Icons.email,color: Theme.of(context).primaryColor,),
-                               hintText: "Enter your email.",
-                               hintStyle: TextStyle(color: Colors.grey, fontSize: 16),
-                             fillColor: Colors.white,
-                             filled: true,
-                           ),
-                         ),
-                         SizedBox(
-                           height: 20,
-                         ),
-                         TextFormField(
-                           controller: passwordController,
-                           decoration: InputDecoration(
-                             enabledBorder: OutlineInputBorder(
-                               borderSide: BorderSide(
-                                 color: Colors.white,
-                                 // width: 2.0
-                               ),
-                                 borderRadius: BorderRadius.circular(10.0),
+                                 child: Text('LOGIN',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 16),),
                              ),
-                             focusedBorder: OutlineInputBorder(
-                               borderRadius: BorderRadius.circular(10.0),
-                               borderSide: BorderSide(
-                                 color: Colors.white,
-                                 // width: 2,
-                               ),
-                             ),
-                             prefixIcon: Icon(Icons.key,color: Theme.of(context).primaryColor,),
-                             hintText: "Enter your password.",
-                             hintStyle: TextStyle(color: Colors.grey,fontSize: 16),
-                             suffixIcon: IconButton(onPressed: (){},icon: Icon(Icons.remove_red_eye),),
-                             suffixIconColor: Theme.of(context).primaryColor,
-                             fillColor: Colors.white,
-                             filled: true,
-
+         
                            ),
-                           obscureText: true,
-                           obscuringCharacter: '*',
-                         ),
-                         SizedBox(
-                          height: 10,
-                        ),
-                         TextButton(onPressed: (){
-// something here
-                         }, child: Text('Forgot Password?',style: TextStyle(color: Theme.of(context).primaryColor , fontSize: 14),)),
-                         SizedBox(
-                           height: 10
-                         ),
-                         Container(
-                           width: 120,
-                           child: ElevatedButton(onPressed: (){
-                             String userEmail = emailController.text.toString();
-                             String userPassword = passwordController.text.toString();
-
-                           //some operation to be done here...
-
-                           },
-                             style: ElevatedButton.styleFrom(
-                               backgroundColor: Theme.of(context).primaryColor,
-                               shape: RoundedRectangleBorder(
-                                 borderRadius: BorderRadius.circular(10)
-                               )
-                             ),
-                               child: Text('Login',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 16),),
+                           SizedBox(
+                               height: 10
                            ),
-
-                         ),
-                         SizedBox(
-                             height: 10
-                         ),
-                         Row(
-                           mainAxisAlignment: MainAxisAlignment.center,
-                           children: [
-                             Text("Don't Have an Account?",style: TextStyle(fontSize: 14, color: Theme.of(context).primaryColor),),
-                             TextButton(onPressed: (){
-// something here
-                             }, child: Text('Register',style: TextStyle(color: hexToColor('#40d900')),))
-                           ],
-                         )
-                       ],
+                           Row(
+                             mainAxisAlignment: MainAxisAlignment.center,
+                             children: [
+                               Text("Don't Have an Account?",style: TextStyle(fontSize: 14, color: Theme.of(context).primaryColor),),
+                               TextButton(onPressed: (){
+                                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> SignupPage(),));
+                               }, child: Text('Register',style: TextStyle(color: hexToColor('#40d900')),))
+                             ],
+                           )
+                         ],
+                       ),
                      ),
                    ),
                  ),
                ),
-             ),
-           ],
+             ],
+           ),
          ),
        ),
      ),
---
+++ lib/signupPage.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:iapply/loginPage.dart';
+
+class SignupPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+   return SignupPageState();
+  }
+}
+
+class SignupPageState extends State<SignupPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+body: Container(
+  height: MediaQuery.of(context).size.height,
+  color: Theme.of(context).primaryColor,
+  child: SingleChildScrollView(
+    child: Container(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(60.0),
+                child: Image.asset("assets/images/img.png"),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).canvasColor,
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Form(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Sign Up',style: TextStyle(color: Theme.of(context).primaryColor, fontSize: 30, fontWeight: FontWeight.w800),),
+                          SizedBox(
+                            height: 20,
+                          ),
+
+                          TextFormField(
+                            decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                )
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor,
+                                  width: 2
+                                ),
+                                borderRadius: BorderRadius.circular(10)
+                              ),
+                              hintText: "Full Name",
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                              fillColor: Colors.white,
+                              filled: true,
+                              prefixIcon: Icon(Icons.person),
+                              prefixIconColor: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Colors.white,
+                                  )
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Theme.of(context).primaryColor,
+                                      width: 2
+                                  ),
+                                  borderRadius: BorderRadius.circular(10)
+                              ),
+                              hintText: "Email",
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                              fillColor: Colors.white,
+                              filled: true,
+                              prefixIcon: Icon(Icons.email),
+                              prefixIconColor: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Colors.white,
+                                  )
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Theme.of(context).primaryColor,
+                                      width: 2
+                                  ),
+                                  borderRadius: BorderRadius.circular(10)
+                              ),
+                              hintText: "Password",
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                              fillColor: Colors.white,
+                              filled: true,
+                              prefixIcon: Icon(Icons.lock),
+                              prefixIconColor: Theme.of(context).primaryColor,
+                              suffixIcon: IconButton(onPressed: (){
+                                // something
+                              }, icon: Icon(Icons.remove_red_eye)),
+                              suffixIconColor: Theme.of(context).primaryColor,
+                            ),
+                            obscureText: true,
+                            obscuringCharacter: '*',
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide(
+                                      color: Colors.white,
+                                    )
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Theme.of(context).primaryColor,
+                                        width: 2
+                                    ),
+                                    borderRadius: BorderRadius.circular(10)
+                                ),
+                                hintText: "Confirm Password",
+                                hintStyle: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                                fillColor: Colors.white,
+                                filled: true,
+                                prefixIcon: Icon(Icons.lock_open),
+                                prefixIconColor: Theme.of(context).primaryColor,
+                                suffixIcon: IconButton(onPressed: (){
+                                }, icon: Icon(Icons.remove_red_eye)),
+                                suffixIconColor: Theme.of(context).primaryColor
+                            ),
+                            obscureText: true,
+                            obscuringCharacter: '*',
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            width: 120,
+                            child: ElevatedButton(onPressed: (){
+
+                            // something to be done
+
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).primaryColor,
+                              foregroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)
+                              )
+                            ), child: Text('SIGN UP'),),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Already have an account?'),
+                              TextButton(onPressed: (){
+                                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LoginPage()));
+                              }, child: Text('Sign in',style: TextStyle(color: hexToColor("#40d900",)),))
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  ),
+),
+    );
+  }
+}
+Color hexToColor(String hexColor) {
+  hexColor = hexColor.replaceAll("#", "");
+  if (hexColor.length == 6) {
+    hexColor = "FF$hexColor"; // Add alpha value if not provided
+  }
+  return Color(int.parse("0x$hexColor"));
+}
