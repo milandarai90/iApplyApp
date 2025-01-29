7e6f67bd8bca417cd614925dd71e475c7655e550
milandarai90 <milandarai90@gmail.com>
2025-01-30 02:06:22 +0545

    json loaded

---
+++ lib/dataModel.dart
@@ -0,0 +1,30 @@
+class Data{
+  final String id;
+  final String name;
+  final String email;
+  final num phone;
+  final String u_district;
+  final String u_municipality;
+  final num u_ward;
+  final String photo;
+  final String course_details;
+  final String course_title;
+  final String class_details;
+  final String class_name;
+  final num students_number;
+  final num seat_numbers;
+  final String status;
+  final String start_time;
+  final String end_time;
+  final String start_date;
+  final String end_date;
+  final String country_details;
+  final String map;
+  final String guidelines;
+  final String process;
+  final String general_country;
+
+  Data(this.id, this.name, this.email, this.phone, this.u_district, this.u_municipality, this.u_ward, this.photo, this.course_details, this.course_title, this.class_details, this.class_name, this.students_number, this.seat_numbers, this.status, this.start_time, this.end_time, this.start_date, this.end_date, this.country_details, this.map, this.guidelines, this.process, this.general_country);
+
+
+}
